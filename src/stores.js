366a06de6a933dd1840d62a6a7ed3334213e9347
Tobias <tobias.jansson@gmail.com>
2020-04-17 17:04:27 +0200

    Småfix

--- src/stores.js
+++ src/stores.js
@@ -4,9 +4,8 @@ export const candata =  readable('', function start(set) {
     const interval = setInterval(async () => {
         const response = await fetch('http://192.168.0.196:4000');
         console.log(response);
-        // var data = await response.json();
-        // console.log(data);
-        set(response);
+        var data = await response.json();
+        set(data);
     }, 2000);
 
     return function stop() {
